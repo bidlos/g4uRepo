56d9d521a286d0acd7f5f91955c91ae9bec2929c
Vladimir <bad851@mail.ru>
2020-11-04 14:34:22 +0300

    last

--- muBot/exchange/function.php
+++ muBot/exchange/function.php
@@ -9,7 +9,7 @@ class database
 
 class exchange extends database
 {
-    public function getСurrency($currency = FALSE, $getValue)
+    public function getСurrency()
     {
         $html = file_get_contents('https://myfin.by/currency/minsk');
 
@@ -18,13 +18,53 @@ class exchange extends database
         $data['list'] = array();
 
         $entry = $doc->find('tr td');
+
         foreach ($entry as $row) {
             $data['list'][] = pq($row)->text();
         }
 
-        $usd = $data['list'][$getValue];
+        $usd = $data['list'];
 
-        $sum = $getValue * $usd;
+
+        return $usd;
+    }
+    public function insertCurrency()
+    {
+        $arr = $this->getСurrency();
+
+        $this->connect->query("INSERT INTO `ts_exchange`
+        (`blr`, `usd`, `eur`, `usd_sell`, `eur_sell`, `zlot`, `zlot_sell`, `update_date`) 
+        VALUES 
+        ('1','" . $arr[1] . "','" . $arr[6] . "','" . $arr[2] . "','" . $arr[7] . "','" . $arr[16] . "','" . $arr[17] . "', '" . date("d-m-Y") . "')");
+    }
+    public function showCurrency($value = FALSE)
+    {
+        $query = $this->connect->query("SELECT * FROM `ts_exchange` ORDER BY `id` DESC");
+
+        foreach ($query as $k) {
+            return $k[$value];
+        }
+
+        return $k;
+    }
+    public function listCurrencyDate()
+    {
+        $query = $this->connect->query("SELECT * FROM `ts_exchange` ORDER BY `id` DESC LIMIT 8");
+
+        foreach ($query as $k) {
+            echo '
+            <li href="#" class="list-group-item list-group-item-action"><b>Дата</b>: ' . $k['update_date'] . ' 
+            <b>USD</b>: ' . $k['usd'] . ' - ' . $k['usd_sell'] . ' 
+            <b>EUR</b>: ' . $k['eur'] . ' - ' . $k['eur_sell'] . ' 
+            <b>Злотые</b>: ' . $k['zlot'] . ' - ' . $k['zlot_sell'] . '</li>';
+        }
+    }
+    public function exschangeCurrency($sell, $buy, $input)
+    {
+        $query = $this->connect->query("SELECT $buy FROM `ts_exchange` ORDER BY `id` DESC");
+        $new = $query->fetch_row();
+
+        $sum = ((float)$sell + (float)$input) * (float)$new['0'];
 
         return $sum;
     }
--- muBot/exchange/index.php
+++ muBot/exchange/index.php
@@ -2,8 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-include_once __DIR__ . 'function.php';
-include_once __DIR__ . '/phpQuery-onefile.php';
+include_once __DIR__ . '/function.php';
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -18,27 +17,60 @@ include_once __DIR__ . '/phpQuery-onefile.php';
 
 <body>
 
-<div class="container">
-<div class="row">
-<div class="col-md-12">
-<form action="#" method="GET">
-                        <h1 style="position: center;">
-                            USD - <?php echo $usd; ?>
-                        </h1>
-</div>
-<div class="col-md-12">
-<input type="text" name="cours" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-<?php if (isset($_GET['exchange'])) {
-    $exchangeClass
-}?>
-</div>
-<div class="col-md-12">
-<button type="submit" name="exchange" class="btn btn-primary mb-2" style="height: 60px;width: 100%;">Пересчитать</button>
-                    </form>
-</div>
-</div>
-</div>
+    <div class="container">
+        <div class="row" style="margin-top: 10%;">
+            <div class="col-md-12">
+                <form action="#" method="GET">
+                    <h3>USD <?php echo $exchangeClass->showCurrency('usd') . ' - ' . $exchangeClass->showCurrency('usd_sell'); ?>
+                        EUR <?php echo $exchangeClass->showCurrency('eur') . ' - ' . $exchangeClass->showCurrency('eur_sell'); ?>
+                        ЗЛОТЫЕ <span class="badge badge-secondary">10</span> <?php echo $exchangeClass->showCurrency('zlot') . ' - ' . $exchangeClass->showCurrency('zlot_sell'); ?></h3>
 
+            </div>
+            <div class="col-md-12" style="margin-top: 50px;">
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="input-group mb-3">
+                            <select class="custom-select" id="inputGroupSelect01" name="sell">
+                                <!-- <option selected>Куплю</option>
+                                <option value="1">USD</option>
+                                <option value="2">EUR</option>
+                                <option value="6">ZLOT</option> -->
+                                <option value="blr" selected>BLR</option>
+                            </select>
+
+                            <select class="custom-select" id="inputGroupSelect01" name="buy">
+                                <option selected>Куплю</option>
+                                <option value="usd">USD</option>
+                                <option value="eur">EUR</option>
+                                <option value="zlot">ZLOT</option>
+                                <option value="blr">BLR</option>
+                            </select>
+                            <div class="col-md-8">
+                            <input type="text" name="cours" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <h3><?php
+                            if(isset($_GET['exchange'])){
+                                echo $exchangeClass->exschangeCurrency('blr', $_GET['buy'], $_GET['cours']);
+                            }
+                        ?></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12" style="margin-top: 50px;">
+                <button type="submit" name="exchange" class="btn btn-primary mb-2" style="height: 60px;width: 100%;">Пересчитать</button>
+                </form>
+            </div>
+
+
+            <div class="col-md-12" style="margin-top: 50px;">
+                <?php $exchangeClass->listCurrencyDate();
+                 ?>
+            </div>
+        </div>
+    </div>
 
 </body>
 
