177d6f4dc7bea387acf47bbc64f32b3b9f9f0fd7
Vladimir <bad851@mail.ru>
2020-11-06 11:56:03 +0300

    del

--- muBot/exchange/cron.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-include_once __DIR__ . '/function.php';
-include_once __DIR__ . '/phpQuery-onefile.php';
-
-$exchangeClass->insertCurrency();
