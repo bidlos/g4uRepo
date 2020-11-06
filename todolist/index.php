0356e66f66164fb720a67c41290ac7a1d2ecb840
Vladimir <bad851@mail.ru>
2020-11-06 16:29:00 +0300

    1
    1

--- todolist/index.php
+++ todolist/index.php
@@ -1,17 +1,3 @@
-<?= 'start'; ?>
-
 <?php
-    class Db {
-        private static $connect = 1;
-        static function dbm()
-        {
-            return 1;
-        }
-        public static function get()
-        {
-            return self::dbm();
-        }
-    }
-
-    echo Db::get();
+include_once '/controller/Main_controller.php/';
 ?>
