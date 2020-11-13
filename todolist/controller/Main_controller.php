6820c26eb18b3be4ada5ce82c849ee775254f59b
Vladimir <bad851@mail.ru>
2020-11-13 17:27:28 +0300

    1

--- todolist/controller/Main_controller.php
+++ todolist/controller/Main_controller.php
@@ -1,1 +1,11 @@
-<?php $test = 'Main Controller'; ?>
+<?php
+// function index()
+// {
+//     include_once($_SERVER['DOCUMENT_ROOT'] . '/todolist/view/main.php');
+// }
+
+// function check()
+// {
+//     include_once($_SERVER['DOCUMENT_ROOT'] . '/todolist/model/main.php');
+//     print_r($arr);
+// }
--- todolist/index.php
+++ todolist/index.php
@@ -1,5 +1,36 @@
 <?php
-include_once '/controller/Main_controller.php/';
+$controller = 'Main_controller';
+$action = 'index';
 
-echo $test;
-?>
+// if(!empty($routes[0])){   
+//     $controller = $routes[0];
+// }
+
+// if(!empty($routes[1])){
+//     $action = $routes[1];
+// }
+
+// $controller_path = $_SERVER['DOCUMENT_ROOT']."/todolist/controller/".$controller.'.php';
+
+// if(file_exists($controller_path)){
+//      include ($controller_path);
+// }else{
+//     Error();
+// }
+// if(function_exists($action)){
+//     $action();
+// }else{
+//     Error();
+// }
+
+if($_SERVER['SCRIPT_NAME'] == '/todolist/main'){
+    echo 'true';
+}
+
+echo $_SERVER['REQUEST_URI'];
+       
+function Error(){
+    header('HTTP/1.x 404 Not Found');
+    header("Status: 404 Not Found");
+    exit('Страница на которую вы пытаетесь попасть, не существует или была удалена!');
+}
